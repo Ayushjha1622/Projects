3860123938c48c614f3cb3ccd7d0f7544a3078fa
Ayush jha <jhaa97204@gmail.com>
2025-10-07 23:27:16 +0530

    new update

--- K72/src/pages/Agence.jsx
+++ K72/src/pages/Agence.jsx
@@ -33,6 +33,12 @@ const Agence = () => {
         end: "top -70%",
         scrub: true,
         pin: true,
+        pinSpacing: true,
+        pinReparent: true,
+        pinType: 'transform',
+        scrub: 1, 
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
         onUpdate: (elem) => {
           let imageIndex;
           if (elem.progress < 1) {
@@ -49,43 +55,24 @@ const Agence = () => {
   });
 
   return (
-    <div>
-      <div className="section1">
-        <div
-          ref={imageDivRef}
-          className="h-[20vw] w-[15vw] overflow-hidden absolute top-96 left-[30vw] rounded-4xl"
-        >
-          <img
-            ref={imageRef}
-            className="h-full w-full object-cover "
-            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
-            alt=""
-          />
+     <div className='parent'>
+      <div id='page1' className='py-1 '>
+        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]'>
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
         </div>
-
-        <div className=" relative font-[font2]">
-          <div className=" mt-[55vh]">
-            <h1 className="text-[18vw] uppercase leading-[17vw] text-center ">
-              Soixan7e <br />
-              Douze
-            </h1>
+        <div className='relative font-[font2]'>
+          <div className='lg:mt-[55vh] mt-[30vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
+              Douze</h1>
           </div>
-
-          <div className="pl-[40%] mt-20">
-            <p className="text-6xl ">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              Notre curiosité nourrit notre créativité. On reste humbles et on
-              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
-              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
-              peut faire de bons chiffres à court terme, mais on la tue à long
-              terme. C’est pour ça qu’on s’engage à donner de la perspective,
-              pour bâtir des marques influentes
-            </p>
+          <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
+            <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
           </div>
         </div>
       </div>
+      <div id='page2' className=" h-screen">
 
-      <div className="section2 h-screen"></div>
+      </div>
     </div>
   );
 };
