f9e556a702ffa04dffa0e1d3d0c93c9c070c51c8
Ayush jha <jhaa97204@gmail.com>
2025-10-07 20:00:10 +0530

    stair animation and image animation

--- K72/src/App.jsx
+++ K72/src/App.jsx
@@ -4,10 +4,44 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Link } from 'react-router-dom'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+
+
+
+  useGSAP(function(){
+
+    const tl = gsap.timeline()
+    tl.from('.stair',{
+      height:0,
+      stagger:{
+        amount: -0.25
+      }
+    })
+
+    tl.to('.stair',{
+      y: '100%',
+      stagger: {
+        amount: -0.25
+      }
+
+    })
+
+  })
   return (
     <div className='text-white'>
+
+     <div className='h-screen w-full fixed z-20 top-0'>
+       <div className='h-full w-full flex fixed z-10 top-0'>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+      </div>
+     </div>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/agence' element={<Agence/>} />
--- K72/src/pages/Agence.jsx
+++ K72/src/pages/Agence.jsx
@@ -1,15 +1,93 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
-const Agencies = () => {
+const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
+  ];
+
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        start: "top 43.6%",
+        end: "top -70%",
+        scrub: true,
+        pin: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(
+              Math.round(elem.progress * imageArray.length)
+            );
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+          imageRef.current.src = imageArray[imageIndex]
+        },
+      },
+    });
+  });
+
   return (
-    <div className='font-[font2]'>
-     <div className='bg-red-500 mt-[50vh]' >
-      <h1 className='text-[18vw] uppercase leading-[15vw] text-center '>Soixan7e <br />
-      Douze
-      </h1>
-     </div>
-    </div>
-  )
-}
+    <div>
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="h-[20vw] w-[15vw] overflow-hidden absolute top-96 left-[30vw] rounded-4xl"
+        >
+          <img
+            ref={imageRef}
+            className="h-full w-full object-cover "
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
 
-export default Agencies
+        <div className=" relative font-[font2]">
+          <div className=" mt-[55vh]">
+            <h1 className="text-[18vw] uppercase leading-[17vw] text-center ">
+              Soixan7e <br />
+              Douze
+            </h1>
+          </div>
+
+          <div className="pl-[40%] mt-20">
+            <p className="text-6xl ">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="section2 h-screen"></div>
+    </div>
+  );
+};
+
+export default Agence;
