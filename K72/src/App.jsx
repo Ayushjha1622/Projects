6c38cd95beae63b747ee46798b5c087cfe326bb0
Ayush jha <jhaa97204@gmail.com>
2025-10-08 14:28:21 +0530

    navbar and hamburger button

--- K72/src/App.jsx
+++ K72/src/App.jsx
@@ -5,6 +5,8 @@ import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { Link } from 'react-router-dom'
 import Stairs from './components/common/stairs'
+import Navbar from './components/Navigation/Navbar'
+import FullScreenNav from './components/Navigation/fullScreenNav'
 
 const App = () => {
   
@@ -13,13 +15,16 @@ const App = () => {
   
   return (
    <div>
-     <div className='text-white'>
+    {/* <Navbar/> */}
+    <FullScreenNav/>
+    
+     {/* <div className='text-white'>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/agence' element={<Agence/>} />
         <Route path='/Projects' element={<Projects/>} />
       </Routes>
-    </div>
+    </div> */}
    </div>
   )
 }
---
+++ K72/src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div id='fullscreennav' className='hidden text-white overflow-hidden h-screen w-full z-50 absolute'>
+      <div className=''>
+        <div className='relative link  border-t-1 border-white'>
+            <h1 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Projects</h1>
+           <div className='moveLink absolute text-black top-0 flex bg-[#D3fd50]'>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             
+           </div>
+        </div>
+        
+        <div className='relative link  border-t-1 border-white'>
+            <h1 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Projects</h1>
+           <div className='moveLink absolute text-black top-0 flex bg-[#D3fd50]'>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             
+           </div>
+        </div>
+        
+        <div className='relative link  border-t-1 border-white'>
+            <h1 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Projects</h1>
+           <div className='moveLink absolute text-black top-0 flex bg-[#D3fd50]'>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             
+           </div>
+        </div>
+        
+        <div className='relative link  border-y-1 border-white'>
+            <h1 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Projects</h1>
+           <div className='moveLink absolute text-black top-0 flex bg-[#D3fd50]'>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             <div className='moveX flex  items-center'>
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="" />
+                <h2 className='font-[font2] text-[8vw] uppercase text-center leading-[0.8] pt-10'>Pour Tout voit</h2>
+                <img className='h-32 rounded-full w-96 shrink-0 object-cover' src=" https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="" />
+            </div>
+             
+           </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
