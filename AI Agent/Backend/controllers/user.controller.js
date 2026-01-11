aa258811faff05377ac94874ad8ba7411d0fe85b
Ayush jha <jhaa97204@gmail.com>
2026-01-11 17:16:00 +0530

    update

--- AI Agent/Backend/controllers/user.controller.js
+++ AI Agent/Backend/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+import User from "../models/user.model.js";
+import redisClient from "../services/redis.service.js";
+
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
@@ -17,3 +20,65 @@ export const createUserController = async (req, res) => {
     res.status(400).send(error.message);
   }
 };
+
+
+export const loginController = async(req, res) =>{
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  try {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email}).select('+password')
+
+    if(!user){
+      res.status(401).json({
+        errors: "Invalid credentials"
+      })
+    }
+
+    const isMatch = await user.isValidPassword(password)
+    if (!isMatch){
+       res.status(401).json({
+        errors: "Invalid credentials"
+      })
+    }
+
+    const token = await user.generateJWT()
+
+    res.status(200).json({user,token})
+
+
+
+    
+  } catch (err) {
+    res.status(400).send(err.message)
+  }
+}
+
+export const profileController = async(req, res) =>{
+  console.log(req.user);
+  res.status(200).json({
+    user: req.user
+  })
+  
+}
+
+
+export const logoutController = async(req, res) =>{
+  try {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1]
+
+    redisClient.set(token, 'logout', 'EX', 60*60*24)
+
+    res.status(200).json({
+      message: "logout successfully"
+    })
+    
+  } catch (error) {
+    console.log(err);
+    res.status(400).send(err.message)
+  }
+}
--- AI Agent/Backend/routes/user.routes.js
+++ AI Agent/Backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
 import { body } from "express-validator";
+import * as authMiddleware from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
@@ -10,5 +11,16 @@ router.post(
   body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
   userController.createUserController
 );
+
+
+
+router.post('/login',
+  body('email').isEmail().withMessage("email must be a valid email address"),
+  body('password').isLength({min:3}).withMessage("password must be at least 3 characters long"),
+  userController.loginController)
+
+router.get('/profile',authMiddleware.authUser,userController.profileController)
+
+router.get('/logout',authMiddleware.authUser,userController.logoutController)
 
 export default router;
