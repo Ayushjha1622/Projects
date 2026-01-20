da0cdc28378aff01bbf46cabeccc17ee3b625b4b
Ayush jha <jhaa97204@gmail.com>
2026-01-20 14:05:28 +0530

    login and register Integrated with Backend

--- AI Agent/Backend/app.js
+++ AI Agent/Backend/app.js
@@ -1,21 +1,24 @@
-import express from "express";
-import morgan from "morgan";
-import connect from "./db/db.js";
-import userRoutes from "./routes/user.routes.js"
-import cookieParser from "cookie-parser";
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 connect();
+
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(cors())
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Ayush Jha");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-export default app;
+export default app; 
